37625b6c27923c0308c891940459e57afc0dc2c8
KPtch <36377768+KPtch@users.noreply.github.com>
2018-04-18 23:41:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var data = require('./respond.json');
-var question = require('./question.json');
+
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
@@ -16,40 +15,10 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 
 
-var timeout = undefined;
 
-var msg = server.post('api/messages', connector.listen());
 
+server.post('api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
-    
-    var req = session.message.text;
-    var resKey = null;
-    var keys = Object.keys(data);
-    for(var i=0; i<keys.length; i++){
-        var key = keys[i];
-        var regex = new RegExp(key);
-        if(req.match(regex)){
-            resKey = key;            
-            break;
-        }
-        
-    }
-    if(resKey){
-        var s = 'นี้จ้า'+"\n";
-        session.send(s+data[resKey];
-//         setTimeout(function(){ session.send(s+data[resKey]) }, 500);
-        
-        
-    } else {
-        
-        var res = 'สวัสดีจ้าา เราคือบอท KunSri'+'\n';
-        question.forEach(function(questions,index){
-            res += "\n"+questions;
-            
-        });
-//         setTimeout(function(){ session.send(res) }, 500);
-        session.send(res);
-    }           
-           
+    session.send("Hello World");
 });
